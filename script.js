ce9168c0f43e85d6284a8e3b51f811ff17b58e70
Hüseyin Ertan <seroka207@gmail.com>
2019-11-01 17:21:05 +0300

    items

--- script.js
+++ script.js
@@ -93,10 +93,10 @@ var combs = {
         "Recurve Bow": "Repeating Crossbow",
         "Needlessly Large Rod": "Jeweled Gauntlet",
         "Tear of the Goddess": "Hand of Justice",
-        "Chain Vest": "Iceborne Gauntlet",
+        "Chain Vest": "Iceborn Gauntlet",
         "Negatron Cloak": "Quicksilver",
         "Giant's Belt": "Trap Claw",
-        "Spatula": "Berserker ",
+        "Spatula": "Berserker Axe",
         "Sparring Glove": "Thief's Gloves"
 
 
@@ -105,6 +105,70 @@ var combs = {
 
   }
 
+var photos = {  
+  "English": [
+    {
+      "B.F. Sword": [ "+15 Attack Damage." , "images\\İtems\\B.F. Sword.png" ],
+      "Chain Vest": [ "+20 Armor" , "images\\İtems\\ChainVestg.png" ],
+      "Giant's Belt": [ "+200 Health" , "images\\İtems\\Giant'sBelt.png" ],
+      "Needlessly Large Rod": [ "+20% Spell Damage" , "images\\İtems\\NeedlesslyLargeRod.png" ],
+      "Negatron Cloak": [ "+20 Magic Resist" , "images\\İtems\\NegatronCloak.png" ],
+      "Recurve Bow": [ "+20% Attack Speed" , "images\\İtems\\RecurveBow.png" ],
+      "Spatula": [ "It must do something..." , "images\\İtems\\Spatula.png" ],
+      "Sparring Glove": [ "+10% Critical Strike Chance. +10% Dodge Chance" , "images\\İtems\\SparringGlove.png" ],
+      "Tear of the Goddess": [ "+20 Starting Mana." , "images\\İtems\\TearoftheGoddess.png" ],
+
+      "Berserker Axe": [ "Wearer is also a Berserker." , "images\\İtems\\BerserkerAxe.png" ],
+      "Blade of the Ruined King": [ "Wearer is also a Blademaster" , "images\\İtems\\BladeoftheRuinedKing.png" ],
+      "The Bloodthirster": [ "Basic Attacks heal the wearer for 40% of the damage dealt." , "images\\İtems\\Bloodthirster.png" ],
+      "Death Blade": [ "Whenever the wearer kills or participates in killing an enemy, gain +15 Attack Damage for the remainder of combat. This effect can stack any number of times." , "images\\İtems\\Deathblade.png" ],
+      "Dragon's Claw": [ "Wearer gains 75% resistance to magic damage." , "images\\İtems\\DragonsClaw.png" ],
+      "Force of Nature": [ "Gain +1 team size" , "images\\İtems\\ForceofNature.png" ],
+      "Frozen Heart": [ "Reduce the Attack Speed of adjacent enemies by 35% for 1 second." , "images\\İtems\\FrozenHeart.png" ],
+      "Frozen Mallet": [ "Wearer is also a Glacial." , "images\\İtems\\FrozenMallet.png" ],
+      "Giant Slayer": [ "The wearer's basic attacks deal an additional 5% of the target's Maximum Health as true damage." , "images\\İtems\\GiantSlayer.png" ],
+      "Guardian Angel": [ "When the wearer dies, they cleanse negative effects and revive with up to 500 Health after a 2 second delay. This effect can trigger once per combat." , "images\\İtems\\GuardianAngel.png" ],
+      "Guinsoo's Rageblade": [ "Basic Attacks grant the wearer +5% bonus Attack Speed for the rest of combat. This effect can stack any number of times." , "images\\İtems\\GuinsoosRageblade.png" ],
+      "Hand of Justice": [ "At the beginning of each planning phase, the wearer gains one of the following:[ Basic Attacks and spells deal +40% Damage or Basic Attacks heal 40 Health on-hit." , "images\\İtems\\HandofJustice.png" ],
+      "Hextech Gunblade": [ "Basic Attacks and spells heal the wearer for 25% of the damage dealt." , "images\\İtems\\HextechGunblade.png" ],
+      "Hush": [ "Basic Attacks have a 33% chance to silence the target on-hit, prevent the enemy from gaining mana for 4 seconds." , "images\\İtems\\Hush.png" ],
+      "Iceborn Gauntlet": [ "When the wearer dodges, they create a zone of ice. Enemies inside the zone have their Attack Speed reduced by 35%." , "images\\İtems\\IceborneGauntlet.png" ],
+      "Inferno's Cinder": [ "Wearer is also an Inferno" , "images\\İtems\\InfernoCinder.png" ],
+      "Infinity Edge": [ "The wearer gains +125% Critical Strike Damage." , "images\\İtems\\InfinityEdge.png" ],
+      "Ionic Spark": [ "Whenever an enemy casts a spell, the wearer deals 125 true damage to the caster." , "images\\İtems\\IonicSpark.png" ],
+      "Jeweled Gauntlet": [ "The wearer's spells can critically strike" , "images\\İtems\\JeweledGauntlet.png" ],
+      "Locket of the Iron Solari": [ "When combat begins, the wearer and all allies within 2 hexes in the same row gain a shield that blocks 300 damage for 8 seconds." , "images\\İtems\\LocketoftheIronSolari.png" ],
+      "Luden's Echo": [ "When the wearer deals damage with their spell, the first target hit and up to 3 nearby enemies are dealt an additional 180 magic damage." , "images\\İtems\\LudensEcho.png" ],
+      "Mage's Cap": [ "Wearer is also a Mage." , "images\\İtems\\MagesCap.png" ],
+      "Morellonomicon": [ "When the wearer deals damage with their spell, they burn the target, dealing 20% of the target's Maximum Health as true damage over 10 seconds and reducing healing by 80% for the duration of the burn. A champion can only have one burn effect at a time, preferring the strongest." , "images\\İtems\\Morellonomicon.png" ],
+      "Phantom Dancer": [ "Wearer dodges all Critical Strikes." , "images\\İtems\\PhantomDancer.png" ],
+      "Quicksilver": [ "The wearer gains a shield that prevents the next crowd control effect from applying. This shield refreshes after 3 seconds." , "images\\İtems\\Quicksilver.png" ],
+      "Rabadon's Deathcap": [ "Wearer gains +50% Spell Power Amplification. (All sources of Spell Power are 50% more effective)" , "images\\İtems\\RabadonsDeathcap.png" ],
+      "Rapid Firecannon": [ "Wearer gains +100% Attack Range." , "images\\İtems\\RapidFirecannon.png" ],
+      "Red Buff": [ "Wearer's Basic Attacks burn the target on-hit, dealing 20% of the target's Maximum Health as true damage over 10 seconds and reducing healing by 80% for the duration of the burn. A champion can only have one burn effect at a time, preferring the strongest." , "images\\İtems\\RedBuff.png" ],
+      "Redemption": [ "When the wearer falls below 30% Health, nearby allies are healed for 1200 Health after a 2.5 second delay. This effect can trigger once per combat." , "images\\İtems\\Redemption.png" ],
+      "Repeating Crossbow": [ "When the wearer dies, Repeating Crossbow is re-equipped to a new ally. Each time Repeating Crossbow is re-equipped, it grants additional +30% Attack Speed and +30% Critical Strike Chance for the remainder of combat. This effect can stack any number of times." , "images\\İtems\\RepeatingCrossbow.png" ],
+      "Runaan's Hurricane": [ "Basic Attacks fire an additional missile at another nearby enemy, dealing 75% of the wearer's Attack damage and applying on-hit effects." , "images\\İtems\\RunaansHurricane.png" ],
+      "Seraph's Embrace": [ "After casting their spell, the wearer restores 20 Mana." , "images\\İtems\\SeraphsEmbrace.png" ],
+      "Spear of Shojin": [ "After casting their spell, the wearer's Basic Attacks restore 18% of their Maximum Mana." , "images\\İtems\\SpearofShojin.png" ],
+      "Statikk Shiv": [ "Every third Basic Attack from the wearer deals 100 magical damage to 3 enemies." , "images\\İtems\\StatikkShiv.png" ],
+      "Sword Breaker": [ "Wearer's Basic Attacks have a 33% chance to disarm the target for 3 seconds, preventing that enemy from Basic Attacking." , "images\\İtems\\SwordBreaker.png" ],
+      "Talisman of Light": [ "Wearer is also a Light." , "images\\İtems\\TalismanofLight.png" ],
+      "Thief's Gloves": [ "At the beginning of each planning phase, the wearer equips 2 temporary items. Temporary items increase in power based on your player level." , "images\\İtems\\ThiefsGloves.png" ],
+      "Thornmail": [ "When the wearer is hit by a Basic Attack, they reflect 100% of the mitigated damage as magic damage." , "images\\İtems\\Thornmail.png" ],
+      "Titanic Hydra": [ "Basic Attacks deal an additional 3% of the wearer's Maximum Health as magic damage to the target and adjacent enemies behind them." , "images\\İtems\\TitanicHydra.png" ],
+      "Trap Claw": [ "At the beginning of combat, the wearer gains a shield that blocks the first enemy spell that hits them. The enemy that breaks the shield is stunned for 4 seconds." , "images\\İtems\\TrapClaw.png" ],
+      "Warden's Mail": [ "Wearer is also a Warden." , "images\\İtems\\WardensMail.png" ],
+      "Warmog's Armor": [ "Wearer regenerates 6% of their missing Health per second." , "images\\İtems\\WarmogsArmor.png" ],
+      "Youmuu's Ghostblade": [ "Wearer is also an Assassin." , "images\\İtems\\YoumuusGhostblade.png" ],
+      "Zeke's Herald": [ "When combat begins, the wearer and all allies within 2 hexes in the same row gain +15% Attack Speed for the rest of combat." , "images\\İtems\\ZekesHerald.png" ],
+      "Zephyr": [ "When combat begins, the wearer summons a whirlwind on the opposite side of the arena that removes the closest enemy from combat for 6 seconds.", "images\\İtems\\Zephyr.png" ]
+
+
+
+
+    }
+  ]}
 
 
 
@@ -121,13 +185,40 @@ var filters = {};
 
 $('.items').on( 'click', '.resimli', function() {
 
+
+  
+$( ".items" ).find('.chosen' ).removeClass("chosen");
+
+$( this ).addClass('chosen');
+
 duo = document.getElementsByClassName("chosen");
 
-document.getElementById("resim").src = combs[0][0];
 
-console.log(duo[1]);
+document.getElementById("resim").src = photos.English[0][combs.English[(duo[0].id).toString()][(duo[1].id).toString()]][1];
+
+
+
 
 });
+
+
+$('.items1').on( 'click', '.resimli', function() {
+
+$( ".items1" ).find('.chosen' ).removeClass("chosen");
+$( this ).addClass('chosen');
+
+duo = document.getElementsByClassName("chosen");
+
+
+document.getElementById("resim").src = photos.English[0][combs.English[(duo[0].id).toString()][(duo[1].id).toString()]][1];
+
+
+
+});
+
+
+
+
 
 $('.heading').on( 'click', '.item', function() {
 
